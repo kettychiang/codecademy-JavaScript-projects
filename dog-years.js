6cd565d7db8b0446162b893c04033056b3873326
Ketty C <105115533+kettychiang@users.noreply.github.com>
2022-10-19 23:01:51 +1100

    Adding dog years project

---
+++ dog-years.js
@@ -0,0 +1,16 @@
+// This is my age
+const myAge = 38;
+// Early years
+let earlyYears = 2;
+// Early years dog age
+earlyYears *= 10.5; 
+// Later years
+let laterYears = myAge -2;
+// Later years dog age
+laterYears *= 4;
+// My age in dog year
+let myAgeInDogYears = earlyYears + laterYears;
+// My name in all lower cases
+let myName= "Ketty Chiang".toLowerCase();
+// Convert my human age to dog age
+console.log(`My name is ${myName}. I amd ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
